f13b896a23971bbca36cc092787d30227a7c473e
Jack <jack.glendenning@gmail.com>
2018-12-16 07:09:39 +1000

    Solve staircase

---
+++ algorithms/staircase.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+
+    for i in range(n):
+        string = " " * (n-i-1) + '#' * (i+1)
+        print(string)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
